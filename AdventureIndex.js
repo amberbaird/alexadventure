5c5618c0fd31e63c7afe987bfb677725e966f72f
metalguy810 <simpsog2@mail.lcc.edu>
2016-02-27 09:15:39 -0500

    Added files via upload

---
+++ AdventureIndex.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+
+'use strict';
+var AlexAdventure = require('./alexAdventure');
+
+exports.handler = function (event, context) {
+    var alexAdventure = new AlexAdventure();
+    alexAdventure.execute(event, context);
+};
