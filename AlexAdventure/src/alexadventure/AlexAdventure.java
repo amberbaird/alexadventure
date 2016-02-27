01ea9aca1ec2c94ed4ee5f6277e04af140133159
amberbaird <mccorma3@mail.lcc.edu>
2016-02-26 19:28:40 -0500

    Added files via upload
    Initial test

---
+++ AlexAdventure/src/alexadventure/AlexAdventure.java
@@ -0,0 +1,12 @@
+package alexadventure;
+
+
+public class AlexAdventure {
+
+    public static void main(String[] args) {
+        
+        //prints a greeting to the world
+        System.out.print("Hello World!");
+    }
+    
+}
