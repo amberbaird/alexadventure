a007aadb608b9170aa7b47b32ec07073afafa57b
metalguy810 <simpsog2@mail.lcc.edu>
2016-02-27 07:07:14 -0500

    Added files via upload

---
+++ AlexAdventure.js
@@ -0,0 +1,25 @@
+// JavaScript source code
+
+'use strict';
+var AlexaSkill = require('./AlexaSkill'),
+    eventHandlers = require('./eventHandlers'),
+    intentHandlers = require('./intentHandlers');
+
+var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]"; not quite sure what this is.
+var skillContext = {};
+
+var AlexAdventure = function () {
+    AlexaSkill.call(this, APP_ID);
+    skillContext.needMoreHelp = true;
+};
+
+
+// Extend AlexaSkill
+AlexAdventure.prototype = Object.create(AlexaSkill.prototype);
+AlexAdventure.prototype.constructor = AlexAdventure;
+
+eventHandlers.register(AlexAdventure.prototype.eventHandlers, skillContext);
+intentHandlers.register(AlexAdventure.prototype.intentHandlers, skillContext);
+
+module.exports = AlexAdventure;
+
