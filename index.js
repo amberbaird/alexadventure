fdbafe4511c7a6b688a71969fa56c787916f91c9
amberbaird <mccorma3@mail.lcc.edu>
2016-02-27 19:18:28 -0500

    Create index.js
    Modifying historybuff index to have a "conversation" for the adventure

---
+++ index.js
@@ -0,0 +1,284 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+/**
+ * This sample shows how to create a Lambda function for handling Alexa Skill requests that:
+ *
+ * - Web service: communicate with an external web service to get events for specified days in history (Wikipedia API)
+ * - Pagination: after obtaining a list of events, read a small subset of events and wait for user prompt to read the next subset of events by maintaining session state
+ * - Dialog and Session state: Handles two models, both a one-shot ask and tell model, and a multi-turn dialog model.
+ * - SSML: Using SSML tags to control how Alexa renders the text-to-speech.
+ *
+ * Examples:
+ * One-shot model:
+ * User:  "Alexa, ask History Buff what happened on August thirtieth."
+ * Alexa: "For August thirtieth, in 2003, [...] . Wanna go deeper in history?"
+ * User: "No."
+ * Alexa: "Good bye!"
+ *
+ * Dialog model:
+ * User:  "Alexa, open History Buff"
+ * Alexa: "History Buff. What day do you want events for?"
+ * User:  "August thirtieth."
+ * Alexa: "For August thirtieth, in 2003, [...] . Wanna go deeper in history?"
+ * User:  "Yes."
+ * Alexa: "In 1995, Bosnian war [...] . Wanna go deeper in history?"
+ * User: "No."
+ * Alexa: "Good bye!"
+ */
+
+
+/**
+ * App ID for the skill
+ */
+var APP_ID = undefined; //replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+
+/**
+ * The AlexaSkill Module that has the AlexaSkill prototype and helper functions
+ */
+var AlexaSkill = require('./AlexaSkill');
+
+
+/**
+ * AlexAdventure is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var AlexAdventure = function() {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+AlexAdventure.prototype = Object.create(AlexaSkill.prototype);
+AlexAdventure.prototype.constructor = AlexAdventure;
+
+AlexAdventure.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("AlexAdventure onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+
+    // any session init logic would go here
+};
+
+AlexAdventure.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("AlexAdventure onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    getWelcomeResponse(response);
+};
+
+AlexAdventure.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+
+    // any session cleanup logic would go here
+};
+
+var AdventureChoice;
+var Choice1;
+var Choice2;
+var Choice3;
+var Choice4;
+
+//start here with logic
+AlexAdventure.prototype.intentHandlers = {
+
+    "GetFirstEventIntent": function (intent, session, response) {
+        handleFirstEventRequest(intent, session, response);
+    },
+
+    "GetNextEventIntent": function (intent, session, response) {
+        handleNextEventRequest(intent, session, response);
+    },
+
+	//help stuff
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        var speechText = "You can use AlexAdventure to play through an adventure. " +
+            "For example, you could say today, or August thirtieth, or you can say exit. Now, which day do you want?";
+        var repromptText = "Which day do you want?";
+        var speechOutput = {
+            speech: speechText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        var repromptOutput = {
+            speech: repromptText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = {
+                speech: "So long, adventurer!",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = {
+                speech: "So long, adventurer!",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.tell(speechOutput);
+    }
+};
+
+/**
+ * Function to handle the onLaunch skill behavior
+ */
+
+function getWelcomeResponse(response) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    var cardTitle = "Go On An Adventure";
+    var repromptText = "AlexAdventure can take you on an adventure. Please choose whether you would like a baseball adventure for kids or a dragon adventure for adults.";
+	
+    var speechText = "<p>Welcome to AlexAdventure.</p> <p>Would you like to go on a kid's baseball adventure or an adult's dragon adventure?</p>";
+    var cardOutput = "AlexAdventure. Go on a baseball adventure or dragon adventure.";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+
+    var speechOutput = {
+        speech: "<speak>" + speechText + "</speak>",
+        type: AlexaSkill.speechOutputType.SSML
+    };
+    var repromptOutput = {
+        speech: repromptText,
+        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+    };
+    response.askWithCard(speechOutput, repromptOutput, cardTitle, cardOutput);
+}
+
+
+
+/**
+ * Gets a poster prepares the speech to reply to the user.
+ */
+function handleFirstEventRequest(intent, session, response) {
+    var daySlot = intent.slots.day;
+    var repromptText = "AlexAdventure can take you on an adventure. What is your name adventurer?";
+	
+	var name = "";
+
+    var sessionAttributes = {};
+    // Read the first 3 events, then set the count to 3
+    sessionAttributes.index = paginationSize;
+
+
+			
+     response.ask(speechOutput, repromptOutput);
+        }
+    });
+}
+
+
+function createPlayerName(name) {
+    return {
+        name: name
+    };
+/**
+ * Gets a poster prepares the speech to reply to the user.
+ */
+function handleNextEventRequest(intent, session, response) {
+    var cardTitle = "More events on this day in history",
+        sessionAttributes = session.attributes,
+        result = sessionAttributes.text,
+        speechText = "",
+        cardContent = "",
+        repromptText = "Do you want to know more about what happened on this date?",
+        i;
+    if (!result) {
+        speechText = "With History Buff, you can get historical events for any day of the year.  For example, you could say today, or August thirtieth. Now, which day do you want?";
+        cardContent = speechText;
+    } else if (sessionAttributes.index >= result.length) {
+        speechText = "There are no more events for this date. Try another date by saying <break time = \"0.3s\"/> get events for august thirtieth.";
+        cardContent = "There are no more events for this date. Try another date by saying, get events for august thirtieth.";
+    } else {
+        for (i = 0; i < paginationSize; i++) {
+            if (sessionAttributes.index>= result.length) {
+                break;
+            }
+            speechText = speechText + "<p>" + result[sessionAttributes.index] + "</p> ";
+            cardContent = cardContent + result[sessionAttributes.index] + " ";
+            sessionAttributes.index++;
+        }
+        if (sessionAttributes.index < result.length) {
+            speechText = speechText + " Wanna go deeper in history?"; //second prompt
+            cardContent = cardContent + " Wanna go deeper in history?";
+        }
+    }
+    var speechOutput = {
+        speech: "<speak>" + speechText + "</speak>",
+        type: AlexaSkill.speechOutputType.SSML
+    };
+    var repromptOutput = {
+        speech: repromptText,
+        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+    };
+    response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
+}
+
+function getJsonEventsFromWikipedia(day, date, eventCallback) {
+    var url = urlPrefix + day + '_' + date;
+
+    https.get(url, function(res) {
+        var body = '';
+
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        res.on('end', function () {
+            var stringResult = parseJson(body);
+            eventCallback(stringResult);
+        });
+    }).on('error', function (e) {
+        console.log("Got error: ", e);
+    });
+}
+
+function parseJson(inputText) {
+    // sizeOf (/nEvents/n) is 10
+    var text = inputText.substring(inputText.indexOf("\\nEvents\\n")+10, inputText.indexOf("\\n\\n\\nBirths")),
+        retArr = [],
+        retString = "",
+        endIndex,
+        startIndex = 0;
+
+    if (text.length == 0) {
+        return retArr;
+    }
+
+    while(true) {
+        endIndex = text.indexOf("\\n", startIndex+delimiterSize);
+        var eventText = (endIndex == -1 ? text.substring(startIndex) : text.substring(startIndex, endIndex));
+        // replace dashes returned in text from Wikipedia's API
+        eventText = eventText.replace(/\\u2013\s*/g, '');
+        // add comma after year so Alexa pauses before continuing with the sentence
+        eventText = eventText.replace(/(^\d+)/,'$1,');
+        eventText = 'In ' + eventText;
+        startIndex = endIndex+delimiterSize;
+        retArr.push(eventText);
+        if (endIndex == -1) {
+            break;
+        }
+    }
+    if (retString != "") {
+        retArr.push(retString);
+    }
+    retArr.reverse();
+    return retArr;
+}
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the HistoryBuff Skill.
+    var skill = new AlexAdventure();
+    skill.execute(event, context);
+};
+
