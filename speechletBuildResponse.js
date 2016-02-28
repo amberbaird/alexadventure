e8aa81b6a325c899addaf25a5d323a350d6663db
metalguy810 <simpsog2@mail.lcc.edu>
2016-02-27 20:21:07 -0500

    Added files via upload

---
+++ speechletBuildResponse.js
@@ -0,0 +1,25 @@
+// AlexaAdventure Speechlet and build response
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        repromptText: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
